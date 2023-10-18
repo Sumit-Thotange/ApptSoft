2e9a8f66bbc3ca9b62b16d267ad1e15d90b925f7
sumit <sumit@SUMIT>
2023-10-18 12:18:37 +0530

    Add project files.

---
+++ ApptSoft/Controllers/DashBoardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApptSoft.Controllers
+{
+    //[Authorize]
+    public class DashBoardController : Controller
+    {
+        // GET: DashBoard
+       
+        public ActionResult DashBoardIndex()
+        {
+            return View();
+        }
+    }
+}
